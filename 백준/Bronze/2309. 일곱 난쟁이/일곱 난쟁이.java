47dda35fbf99df397c6a676a856e0304fc78d86b
고지영 Jiyeong Koh <69239555+twinklecherry@users.noreply.github.com>
2025-04-03 22:56:10 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,39 +1,49 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
-public class Main {
+
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] dwarfs = new int[9];
-        int sum = 0;
-
-        // 입력 및 전체 합 계산
-        for (int i = 0; i < 9; i++) {
-            dwarfs[i] = Integer.parseInt(br.readLine());
-            sum += dwarfs[i];
+        ArrayList<Integer> list = new ArrayList<>();
+        int height = 100;
+        int totSum = 0;
+        
+        for(int i=0; i<9; i++){
+            int a = Integer.parseInt(br.readLine());
+            list.add(a);
+            totSum += a;
         }
+        
+        Collections.sort(list);
 
-        // 두 난쟁이 선택 및 합 검증
-        for (int i = 0; i < 8; i++) {
-            for (int j = i + 1; j < 9; j++) {
-                if (sum - dwarfs[i] - dwarfs[j] == 100) {
-                    // 일곱 난쟁이 출력
-                    int[] result = new int[7];
-                    int index = 0;
-                    for (int k = 0; k < 9; k++) {
-                        if (k != i && k != j) {
-                            result[index++] = dwarfs[k];
-                        }
-                    }
-                    Arrays.sort(result); // 오름차순 정렬
-                    for (int dwarf : result) {
-                        System.out.println(dwarf);
-                    }
-                    return; // 결과 출력 후 종료
-                }
+        int height2 = totSum-height;
+        int start = 0;
+        int end = list.size()-1;
+
+        while(start!=end){
+            int sum = 0;
+            sum += list.get(start);
+            sum += list.get(end);
+
+            if(sum < height2){
+                start++;
+            }
+            if(sum > height2){
+                end--;
+            }
+            if(sum == height2){
+                list.remove(end);
+                list.remove(start);
+                break;
             }
         }
+
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
     }
 }
