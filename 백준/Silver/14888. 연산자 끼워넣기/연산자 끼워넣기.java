01394bf57a26239b9525e0945f136a64ad7141e3
고지영 Jiyeong Koh <69239555+twinklecherry@users.noreply.github.com>
2025-04-08 23:05:40 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 72 ms, Memory: 13356 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,59 +1,57 @@
 import java.util.*;
+import java.io.*;
 
-class Main {
-    static int N;
-    static int[] numbers;
-    static int[] operators = new int[4]; // +, -, *, /
-    static int maxResult = Integer.MIN_VALUE;
-    static int minResult = Integer.MAX_VALUE;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        numbers = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            numbers[i] = sc.nextInt();
-        }
-
-        for (int i = 0; i < 4; i++) {
-            operators[i] = sc.nextInt(); // 연산자 개수 입력
-        }
-
-        // 백트래킹 수행
-        dfs(1, numbers[0]);
-
-        // 최댓값과 최솟값 출력
-        System.out.println(maxResult);
-        System.out.println(minResult);
-    }
-
-    static void dfs(int index, int result) {
-        if (index == N) { // 모든 숫자를 사용했을 때
-            maxResult = Math.max(maxResult, result);
-            minResult = Math.min(minResult, result);
-            return;
-        }
-
-        for (int i = 0; i < 4; i++) {
-            if (operators[i] > 0) { // 해당 연산자가 남아있는 경우
-                operators[i]--; // 연산자 사용
-
-                int newResult = calculate(result, numbers[index], i);
-                dfs(index + 1, newResult); // 다음 숫자로 이동
-
-                operators[i]++; // 연산자 복구 (백트래킹)
-            }
-        }
-    }
-
-    static int calculate(int a, int b, int op) {
-        switch (op) {
-            case 0: return a + b; // 덧셈
-            case 1: return a - b; // 뺄셈
-            case 2: return a * b; // 곱셈
-            case 3: return a / b; // 나눗셈 (정수 나눗셈)
-        }
-        return 0;
-    }
+class Main{
+	static int N;
+	static int minV = Integer.MAX_VALUE;
+	static int maxV = Integer.MIN_VALUE;
+	static int[] nums;
+	static int[] cal; // +, -, *, /
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine()); //수의 개수
+		StringTokenizer st;
+		
+		nums = new int[N];
+		cal = new int[4];
+		
+		//nums 데이터 저장
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		//cal 데이터 저장
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<4; i++) {
+			cal[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		calculate(1, nums[0]);// 첫 번째 수부터 시작
+		
+		System.out.println(maxV);
+		System.out.println(minV);
+		
+	}
+	
+	static void calculate(int idx, int result) {
+		if(idx==N) {
+			maxV = Math.max(maxV, result);
+			minV = Math.min(minV, result);
+			return;
+		}
+		
+		for(int i=0; i<4; i++) {
+			if(cal[i]>0) {
+				cal[i]--;
+				if(i==0) 	  calculate(idx+1, result+nums[idx]);
+				else if(i==1) calculate(idx+1, result-nums[idx]);
+				else if(i==2) calculate(idx+1, result*nums[idx]);
+				else if(i==3) calculate(idx+1, result/nums[idx]);
+				cal[i]++;
+			}
+		}
+	}
 }
